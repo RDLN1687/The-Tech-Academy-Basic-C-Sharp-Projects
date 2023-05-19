717844e280e3b02c66ab35957f6be5095d64a80a
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-05-18 23:22:22 -0600

    Add files via upload

---
+++ Csharp Assignment/Csharp Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_Assignment
+{
+    static class Program
+    {
+          static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
