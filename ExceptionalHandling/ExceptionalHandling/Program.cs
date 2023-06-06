2a5b4fe8afb05a538e82108a7658254198ad0ada
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-05 23:31:30 -0600

    Add files via upload

---
+++ ExceptionalHandling/ExceptionalHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+    class program
+    {
+
+        static void Main(string[] args)
+
+
+        {
+            try
+        {
+            Console.WriteLine("Please select a number between 1-10");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please select another number between 1-10");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            //divide those two numbers which were inputted//
+            int Z = X / Y;
+            Console.WriteLine(Z);
+            Console.ReadLine();
+        }
+        //try and catch method//
+            catch(FormatException)
+                {
+                Console.WriteLine("Please enter a valid number");
+                }
+            catch(DivideByZeroException)
+                {
+                Console.WriteLine("You cannot divide by zero");
+                }
+        }
+
+  
+
+}
