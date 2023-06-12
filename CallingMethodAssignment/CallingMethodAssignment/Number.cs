e3912b4b947d109337ff3fec421790e0b09ba305
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-12 00:18:35 -0600

    Add files via upload

---
+++ CallingMethodAssignment/CallingMethodAssignment/Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CallingMethodAssignment
+{
+    public class Math
+    {
+       public static int Multiply(int Mul)
+        {
+            int Multiplication = Mul * 12;
+            return Multiplication;
+        }
+
+        public static int Addition(int add)
+        {
+            int Sum = add + 12;
+            return Sum;
+        }
+
+        public static int Division(int Div)
+        {
+            int divide = Div / 2;
+            return divide;
+        }
+    }
+}
---
+++ CallingMethodAssignment/CallingMethodAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CallingMethodAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let us do some basic math");
+            Console.WriteLine("Input some number for multiplication");
+            int Mul = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The answer is " + Math.Multiply(Mul));//calling method//
+            Console.ReadLine();
+            Console.WriteLine("Input a number for addition");
+            int add = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The answer is " + Math.Addition(add)); //Calling method//
+            Console.ReadLine();
+            Console.WriteLine("Enter any even number");
+            int Div = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The anwer is " + Math.Division(Div));//Calling method//
+            Console.ReadLine();
+        }
+
+       
+    }
+}
