371ce6b1dc0167753c1ba73405564184d4ea3473
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-13 00:11:39 -0600

    Add files via upload

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MainMethodAssignment
+{
+    public class MathMethod
+    {
+        
+         public static int MathOps(int number)
+            {
+                return number + 45;  
+            }
+
+         public static int MathOps(Decimal number)
+        {
+            int newnum = Convert.ToInt32(number);
+            return newnum - 3;
+        }
+
+         public static int MathOps(string number)
+        {
+            int newnum = Convert.ToInt32(number);
+            return newnum % 5;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //instantiate the Mathmethod Classs//
+            MathMethod math1 = new MathMethod();
+
+            Console.WriteLine("Let us learn about Main Method");
+            Console.WriteLine("Input an integer");
+            //calling each method//
+            Console.WriteLine(MathMethod.MathOps(65));
+            Console.ReadLine();
+            Console.WriteLine(MathMethod.MathOps(45.6m));
+            Console.ReadLine();
+            Console.WriteLine(MathMethod.MathOps("34"));
+            Console.ReadLine();
+            
+        }
+    }
+}
