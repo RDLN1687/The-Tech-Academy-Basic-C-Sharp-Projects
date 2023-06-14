79142acbc60359c2d6b07d38cbe2fdbfddfe9f93
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-13 23:11:32 -0600

    Add files via upload

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+
+        {
+
+            MathOperation mathop = new MathOperation();
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Either enter a second number or press enter");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = MathOperation.Mathoperation1(num1, num2);
+                Console.WriteLine(num1 + " + " + num2 + " = " + results);
+            }
+
+            catch
+            {
+                int result = MathOperation.MathOperation1(num1);
+                Console.WriteLine(num1 + " + Default 3 = " + result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
