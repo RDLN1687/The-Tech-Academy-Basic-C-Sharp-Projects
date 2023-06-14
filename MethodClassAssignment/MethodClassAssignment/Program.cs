271a5d2c21de8dca095a435efb3bf0ef4e9592e4
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-14 00:45:27 -0600

    Add files via upload

--- MethodClassAssignment/MethodClassAssignment/Program.cs
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -4,20 +4,20 @@ namespace MethodClassAssignment
 {
     class MainClass
     {
-
-
+        Mathoperation math = new Mathoperation();
+        
         static void Main(string[] args)
-        {
-            Mathoperation math = new Mathoperation
 
-            {
-               Console.WriteLine("Input a number for addition");
-               int add1 = Convert.ToInt32(Console.ReadLine());
-               Console.WriteLine("Input a number for addition");
-               int add2 = Convert.ToInt32(Console.ReadLine());
-               int result = Mathoperation.Addition(add1, add2); //Calling method//
-               Console.WriteLine("The answer is " + result); 
-               Console.ReadLine();
+        {
+            Console.WriteLine("Input a number for addition");//int1//
+            int add1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a number for addition");//int2//
+            int add2 = Convert.ToInt32(Console.ReadLine());
+            int result = Mathoperation.Addition(add1, add2); //Calling method//
+            Console.WriteLine("The answer is " + result);
+            Console.ReadLine();
         }
+
     }
-}
+        }
+  
