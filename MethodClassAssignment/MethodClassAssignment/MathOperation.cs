7201a80ea925f850191a93f7592f79034085334c
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-15 23:35:57 -0600

    Add files via upload

---
+++ MethodClassAssignment/MethodClassAssignment/MathOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MethodClassAssignment
+{
+    public class MathOperation
+    {
+        public void Addition(int add1, int add2)
+        {
+            int Sum = add1 + add2;
+            Console.WriteLine(Sum);
+        }
+    }
+}
+
+
--- MethodClassAssignment/MethodClassAssignment/Program.cs
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -4,20 +4,18 @@ namespace MethodClassAssignment
 {
     class MainClass
     {
-        Mathoperation math = new Mathoperation();
         
-        static void Main(string[] args)
-
+        public static void Main(string[] args)
+            
         {
+            MathOperation MathOp = new MathOperation();
+
             Console.WriteLine("Input a number for addition");//int1//
             int add1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input a number for addition");//int2//
             int add2 = Convert.ToInt32(Console.ReadLine());
-            int result = Mathoperation.Addition(add1, add2); //Calling method//
-            Console.WriteLine("The answer is " + result);
-            Console.ReadLine();
+            MathOp.Addition(add1, add2); //Calling method//
         }
 
     }
-        }
-  
+}
