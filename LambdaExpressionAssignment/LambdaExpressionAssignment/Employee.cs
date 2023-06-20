7e2c8decbcf6abce7289b632b423eb0696afea7c
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-19 23:33:28 -0600

    Add files via upload

---
+++ LambdaExpressionAssignment/LambdaExpressionAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LambdaExpressionAssignment
+{
+    public class Employee
+    {
+       
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        
+    }
+}
---
+++ LambdaExpressionAssignment/LambdaExpressionAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpressionAssignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+        {
+            new Employee { Id = 1, FirstName = "Joe", LastName = "Smith" },
+            new Employee { Id = 2, FirstName = "John", LastName = "Doe" },
+            new Employee { Id = 3, FirstName = "Jane", LastName = "Smith" },
+            new Employee { Id = 4, FirstName = "Joe", LastName = "Johnson" },
+            new Employee { Id = 5, FirstName = "Sarah", LastName = "Williams" },
+            new Employee { Id = 6, FirstName = "Alex", LastName = "Brown" },
+            new Employee { Id = 7, FirstName = "David", LastName = "Jones" },
+            new Employee { Id = 8, FirstName = "Joe", LastName = "Davis" },
+            new Employee { Id = 9, FirstName = "Emily", LastName = "Taylor" },
+            new Employee { Id = 10, FirstName = "Michael", LastName = "Clark" }
+        };
+
+            List<Employee> joes = new List<Employee>();
+
+            foreach (Employee employee in employees)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+
+            Console.WriteLine("Employees with the first name 'Joe':");
+            foreach (Employee joeEmployee in joes)
+            {
+                Console.WriteLine($"Id: {joeEmployee.Id}, Name: {joeEmployee.FirstName} {joeEmployee.LastName}");
+            }
+
+            List<Employee> joes2 = employees.Where(x => x.FirstName == "Joe").ToList();
+            Console.ReadLine();         
+        }
+    }
+}
