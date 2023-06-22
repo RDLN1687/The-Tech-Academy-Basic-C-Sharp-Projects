1b1fd92bbcbebafb454e1c0c1f087f110b68f3a7
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-22 09:22:42 -0600

    Add files via upload

---
+++ twentyOne/twentyOne/Deck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Spade", "Diamond" };
+            List<string> Faces = new List<string>() { "Ace", "Two", "Three", "Four", "Five", "Six",
+                                                    "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Jack"};
+
+            foreach (string face in Faces)
+            {
+                foreach (string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+        }
+
+        public List<Card> Cards { get; set; }
+
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times =3)
+        {
+            timesShuffled = 0;
+            for (int i = 0; i< times; i++)
+            {
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
+            }
+            return deck;
+        }
+    }
+}
---
+++ twentyOne/twentyOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace twentyOne
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Lets start with your name. ");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("How much money did you bring in today? ");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0} would you like to play the Game of 21 now? ", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new twentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance >0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+
+            Console.WriteLine("Please feel free to look around the casino, Bye for now!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
