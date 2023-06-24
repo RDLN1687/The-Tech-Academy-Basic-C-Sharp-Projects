4e0b0c8bc35cd07f8a7fedc7b8fa9aa4dbeaa5f4
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-06-23 20:34:51 -0600

    Add files via upload

---
+++ InputTextAssignment/InputTextAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Please enter a number: ");
+        string input = Console.ReadLine();
+
+        // Log the number to a text file
+        string fileName = "number_log.txt";
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            writer.WriteLine(input);
+            writer.WriteLine(DateTime.Now);
+        }
+
+        Console.WriteLine("Number logged successfully!");
+
+        // Read and print the contents of the text file
+        Console.WriteLine("Contents of the log file:");
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string content = reader.ReadToEnd();
+            Console.WriteLine(content);
+        }
+    }
+}
